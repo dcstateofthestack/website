8d944066456ee9126ed12ceaefbc22c4880c7e13
Kevin Long <klong@Kevins-Mac-mini.local>
2026-02-08 09:21:08 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    build: {
+        format: 'directory'
+    },
+    vite: {
+        plugins: [tailwindcss()],
+        server: {
+            fs: {
+                strict: false
+            }
+        }
+    },
+});
